57eb6ad1a5187f26c485b94db7a5f05a9c872620
jiho-yang <jhyang053@gmail.com>
2025-11-26 16:39:45 +0900

    edit hollys layout

--- project/hollys/index.js
+++ project/hollys/index.js
@@ -1,75 +1,65 @@
 $(document).ready(function () {
-    $('.single-item').slick({
-        arrows: false,         // ← 이전/다음 버튼 안 보이게!
-        dots: true,            // ← 점(dot) 보이게!
-        infinite: true,
-        slidesToShow: 1,
-        slidesToScroll: 1,
-        autoplay: true,
-        autoplaySpeed: 3000,
-        pauseOnHover: false,
-    });
+  $('.single-item').slick({
+    arrows: false,
+    dots: true,
+    infinite: true,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 3000,
+    pauseOnHover: false,
+  });
 
+  const $center = $('.center');
 
-    // $('.center').slick({
-    //     centerMode: true,
-    //     centerPadding: '60px',
-    //     slidesToShow: 3,
-    //     prevArrow: '<button class="custom-prev"><</button>',
-    //     nextArrow: '<button class="custom-next">></button>',
-    //     responsive: [
-    //         {
-    //             breakpoint: 768,
-    //             settings: {
-    //                 arrows: false,
-    //                 centerMode: true,
-    //                 centerPadding: '40px',
-    //                 slidesToShow: 3
-    //             }
-    //         },
-    //         {
-    //             breakpoint: 480,
-    //             settings: {
-    //                 arrows: false,
-    //                 centerMode: true,
-    //                 centerPadding: '40px',
-    //                 slidesToShow: 1
-    //             }
-    //         }
-    //     ]
-    // });
+  // 공통 함수: 특정 인덱스 슬라이드에만 텍스트 활성화
+  function setMallCaptionActive(slick, index) {
+    $('.center .mall-txt-box').removeClass('is-active');
+    $(slick.$slides[index]).find('.mall-txt-box').addClass('is-active');
+  }
 
+  $center
+    // 처음 로드될 때 중앙 슬라이드 텍스트 켜기
+    .on('init', function (event, slick) {
+      setMallCaptionActive(slick, slick.currentSlide);
+    })
 
+    // ✨ 이동 "시작"할 때 바로 텍스트 교체
+    .on('beforeChange', function (e, slick, cur, next) {
+      setMallCaptionActive(slick, next);  // ← 여기서 바로 텍스트 교체
 
-    $('.center').slick({
-        centerMode: true,
-        centerPadding: '0',
-        slidesToShow: 3,
-        arrows: true,
-        prevArrow:
-            '<button type="button" class="slick-prev" aria-label="Previous">' +
-            '<i class="bi bi-arrow-left-circle default"></i>' +
-            '<i class="bi bi-arrow-left-circle-fill hover"></i>' +
-            '</button>',
-        nextArrow:
-            '<button type="button" class="slick-next" aria-label="Next">' +
-            '<i class="bi bi-arrow-right-circle default"></i>' +
-            '<i class="bi bi-arrow-right-circle-fill hover"></i>' +
-            '</button>',
-        speed: 450,
-        cssEase: 'ease'
-    });
+      // 래핑(is-wrap) 처리도 같이 유지
+      const last = slick.slideCount - 1;
+      const isWrap =
+        (cur === last && next === 0) ||
+        (cur === 0 && next === last);
+      if (isWrap) $(this).addClass('is-wrap');
+    })
 
-    $center.on('beforeChange', function (e, slick, cur, next) {
-        const last = slick.slideCount - 1;
-        const isWrap = (cur === last && next === 0) || (cur === 0 && next === last);
-        if (isWrap) $(this).addClass('is-wrap');
-    });
+    .on('afterChange', function () {
+      $(this).removeClass('is-wrap');
+    })
 
-    $center.on('afterChange', function () {
-        $(this).removeClass('is-wrap');
+    .slick({
+      centerMode: true,
+      centerPadding: '0',
+      slidesToShow: 3,
+      arrows: true,
+      prevArrow:
+        '<button type="button" class="slick-prev" aria-label="Previous">' +
+        '<i class="bi bi-arrow-left-circle default"></i>' +
+        '<i class="bi bi-arrow-left-circle-fill hover"></i>' +
+        '</button>',
+      nextArrow:
+        '<button type="button" class="slick-next" aria-label="Next">' +
+        '<i class="bi bi-arrow-right-circle default"></i>' +
+        '<i class="bi bi-arrow-right-circle-fill hover"></i>' +
+        '</button>',
+      speed: 450,
+      cssEase: 'ease'
     });
 });
+
 
 
 
